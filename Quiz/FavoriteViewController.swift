ab33700008fbed580824f83dae69abb357d2aef9
tatsiana <tanya3308225@mail.ru>
2021-12-20 20:06:34 +0300

    was added seva functionality

---
+++ Quiz/FavoriteViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FavoriteViewController.swift
+//  Quiz
+//
+//  Created by Tatsiana Marchanka on 20.12.21.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
